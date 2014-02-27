120b04cd9a960d47fb5c79b10deb400d28f0d049
nino2armando <nino2armando@yahoo.com>
2014-02-26 23:42:20 -0800

    first draft on bubble sort

---
+++ Algorithms/BubbleSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class BubbleSort
+    {
+        public static int[] Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 1, 4, 3, 7, 44, 34, 6, 11, 36, 43 };
+            var sorted = BubbleSort.Sort(array);
+
+            foreach(int item in sorted)
+            {
+                Console.Write(item.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
