9d4f523fbfd2bb09514e3df3b46634b3700f2522
Nino Khodabandeh <nkhodabandeh@paybyphone.com>
2014-03-19 10:41:23 -0700

    added Bsearch

---
+++ Algorithms/BinarySearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class BinarySearch
+    {
+        public static int BSearch(int index, int[] arraryToSearch)
+        {
+            int lowIndex = 0;
+            int highIndex = arraryToSearch.Length;
+
+            if (index < arraryToSearch[lowIndex])
+                return -1;
+            if (index > arraryToSearch[highIndex - 1])
+                return -1;
+
+            while (lowIndex <= highIndex )
+            {
+                int mid = (lowIndex + highIndex )/2;
+
+                if (index > arraryToSearch[mid])
+                {
+                    lowIndex = mid + 1;
+                }
+                else if(index < arraryToSearch[mid])
+                {
+                    highIndex = mid - 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -10,6 +10,7 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            // bubble sort 
             var array = new int[] { 1, 4, 3, 7, 44, 34, 6, 11, 36, 43 };
             var sorted = BubbleSort.Sort(array);
 
@@ -18,6 +19,12 @@ namespace Algorithms
                 Console.Write(item.ToString());
             }
 
+            // binary search
+
+            var myArray = new int[] {1, 2, 4, 6, 10, 14, 15, 19, 20, 34, 36, 38, 40, 42};
+
+            var value = BinarySearch.BSearch(10, myArray);
+            
             Console.ReadKey();
         }
     }
